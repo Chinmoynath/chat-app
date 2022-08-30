04bf5cc07e511d83dc61470b7d725f1a61ced3ce
Chinmoy <nathchinmoy777@gmail.com>
2022-08-30 23:42:33 +0530

    upadated nickname inside the database

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -20,7 +20,7 @@ export const ProfileProvider = ({ children }) => {
       if (authObj) {
         userRef = database.ref(`/profiles/${authObj.uid}`);
 
-        userRef.on("value", (snap) => {
+        userRef.on("value", snap => {
           const { name, createdAt } = snap.val();
 
           const data = {
